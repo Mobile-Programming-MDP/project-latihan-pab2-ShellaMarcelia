07a86fc454dac77b089a771cd67e85f1ce1fddbd
ShellaMarcelia <160854951+ShellaMarcelia@users.noreply.github.com>
2025-03-28 02:05:35 +0700

    home screen

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,16 +1,34 @@
+import 'package:fasum/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  Future<void> signOut(BuildContext context)async{
+    await FirebaseAuth.instance.signOut();
 
-class _HomeScreenState extends State<HomeScreen> {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context)=> SignInScreen())
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title:  const Text("Home"),
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      actions: [
+        IconButton(onPressed: (){
+          signOut(context);
+        }, icon: const Icon(Icons.logout),
+        ),
+      ],
+      ),
+      body: const Center(
+        child: Text("You have logged in"),
+      ),
+    );
   }
 }
